c1e187d296baffb0bafe75d4cff88691b83249c0
zhyi <zhyi@zhyi-mbp128.local>
2026-09-07 18:31:48 +0800

    fix(formula): specify explicit architecture dependencies for macOS and Linux

--- Formula/trg.rb
+++ Formula/trg.rb
@@ -5,28 +5,24 @@ class Trg < Formula
   license "Apache-2.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/tokalang/trg/releases/download/v0.15.0/trg-v0.15.0-macos-arm64.tar.gz"
-      sha256 "25929b41e4cad1572e4e933bc484a24a07b247fcedd9d1300cd5ec1bd934fde4"
+    depends_on arch: :arm64
 
-      def install
-        bin.install "trg"
-      end
-    end
+    url "https://github.com/tokalang/trg/releases/download/v0.15.0/trg-v0.15.0-macos-arm64.tar.gz"
+    sha256 "25929b41e4cad1572e4e933bc484a24a07b247fcedd9d1300cd5ec1bd934fde4"
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/tokalang/trg/releases/download/v0.15.0/trg-v0.15.0-linux-x64.tar.gz"
-      sha256 "b39ec25f38a39919091a62ec70783d5f8d0187b152244d4841c237231171c415"
+    depends_on arch: :x86_64
 
-      def install
-        bin.install "trg"
-      end
-    end
+    url "https://github.com/tokalang/trg/releases/download/v0.15.0/trg-v0.15.0-linux-x64.tar.gz"
+    sha256 "b39ec25f38a39919091a62ec70783d5f8d0187b152244d4841c237231171c415"
+  end
+
+  def install
+    bin.install "trg"
   end
 
   test do
-    assert_match "trg 0.15.0", shell_output("#{bin}/trg --version")
+    assert_match "trg #{version}", shell_output("#{bin}/trg --version")
   end
 end
